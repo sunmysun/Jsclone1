7413eb0df1ddc93994f64d2bb671cfdfc0f527f4
sh_sun@naver.com <81813275+sunmysun@users.noreply.github.com>
2021-12-21 09:46:51 +0900

    condition

--- Jsclone1/js/asdf.js
+++ Jsclone1/js/asdf.js
@@ -1,19 +1,13 @@
-const calculator = {
-    add: function(a,b){
-        console.log(a+b);
-    },
-    minus: function(a,b){
-        console.log(a-b);
-    },
-    divide:function(a,b){
-        console.log(a/b);
-    },
-    multiple:function(a,b){
-        console.log(a**b);
-    }
-};
+const age = parseInt(prompt("how old are you?"));
 
-calculator.add(2,3);
-calculator.minus(2,3);
-calculator.divide(6,3);
-calculator.multiple(2,3);
+if(isNaN(age) || age < 0){
+    console.log("Please write a real positive number");
+} else if (age < 18) {
+    console.log("you are too young");
+} else if (age>=18 && age<=50){
+    console.log('you can drink');
+} else if (age>50 && age<=80){
+    console.log('you should not drink');
+} else if(age>80){
+    console.log("you can do whatever you want");
+}
