c05aaabc2d70ff3ea0c38f85ed0164c33b69778c
sh_sun@naver.com <81813275+sunmysun@users.noreply.github.com>
2021-12-21 11:07:18 +0900

    eventListener

--- Jsclone1/js/asdf.js
+++ Jsclone1/js/asdf.js
@@ -1,13 +1,10 @@
-const age = parseInt(prompt("how old are you?"));
+const title = document.querySelector(".hello:first-child h1");
 
-if(isNaN(age) || age < 0){
-    console.log("Please write a real positive number");
-} else if (age < 18) {
-    console.log("you are too young");
-} else if (age>=18 && age<=50){
-    console.log('you can drink');
-} else if (age>50 && age<=80){
-    console.log('you should not drink');
-} else if(age>80){
-    console.log("you can do whatever you want");
+function handleTitleClick(){
+    const ranColor = ["blue","red","green","yellow","black"];
+    let num = Math.floor(Math.random() * ranColor.length);
+    title.style.color = ranColor[num];
 }
+
+title.addEventListener("click", handleTitleClick);
+
