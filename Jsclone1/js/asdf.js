742644b922ddcea23b17e1ff062e87a3284ccb53
sh_sun@naver.com <81813275+sunmysun@users.noreply.github.com>
2021-12-22 21:59:02 +0900

    #3.8

--- Jsclone1/js/asdf.js
+++ Jsclone1/js/asdf.js
@@ -1,13 +1,13 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else{
-        newColor = "blue";
+    const clickedClass = "clicked";
+    if(h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass);
     }
-    h1.style.color = newColor;
+    else{
+        h1.classList.add(clickedClass);
+    }
 }
+
 h1.addEventListener("click", handleTitleClick);
