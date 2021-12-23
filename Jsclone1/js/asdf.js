55b4b6ea3b047144ea18e655656b3405174b102b
sh_sun@naver.com <81813275+sunmysun@users.noreply.github.com>
2021-12-23 20:29:55 +0900

    #4.6

--- Jsclone1/js/asdf.js
+++ Jsclone1/js/asdf.js
@@ -1,13 +1,16 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function btnclick(){
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    if (username ===""){
-        alert("Please Write your Name.");
-    } else if(username.length > 15){
-        alert("Your name is too long.");
-    }
+    localStorage.setItem("username", username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", btnclick);
+loginForm.addEventListener("submit", onLoginSubmit);
