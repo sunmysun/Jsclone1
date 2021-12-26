8c168b64ac0ddccafb432383ff03d61c8ef4fca7
sh_sun@naver.com <81813275+sunmysun@users.noreply.github.com>
2021-12-26 23:34:18 +0900

    #8.1

--- Jsclone1/js/weather.js
+++ Jsclone1/js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "71f21307304713d30cfca28e5d85bb1d";
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const weatherContainer = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            
+        });
 }
 function onGeoError(){
     alert("Can't find you. No Weather for you.");
